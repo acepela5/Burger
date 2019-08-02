3599ba0952c3b3fbe504352e89d569dde6efae8c
unknown <galan1451@gmail.com>
2019-08-01 21:54:04 -0500

    heroku deployed

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INTEGER AUTO-INCREMENT,
+    id INTEGER auto-increment,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN NOT NULL,
 PRIMARY KEY (id)
